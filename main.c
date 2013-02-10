65ec8005f96b8935c984ea5e16cd5544d207ca43
chensiyuan <chensiyuan2010@gmail.com>
2013-02-10 14:23:51 -0500

    first

---
+++ main.c
@@ -0,0 +1,43 @@
+/*
+ * main.c
+ *
+ *  Created on: Feb 10, 2013
+ *      Author: siyuanch
+ */
+int v=4;
+int matrix[][] ;
+
+read_Edge(){
+
+}
+
+
+int main(){
+
+int v=4;
+// read the input file//the edge.
+	int i,j;
+	for(i=0; i<v;i++){
+		for(j=0;i<v;j++){
+			read_Edge(d);
+		}
+	}
+
+	printf("hello");
+
+}
+
+
+
+void output(){
+	int i,j;
+		for(i=0; i<v;i++){
+			for(j=0;i<v;j++){
+				printf("%d ",matrix[i][j]);
+			}
+		}
+}
+
+
+
+
